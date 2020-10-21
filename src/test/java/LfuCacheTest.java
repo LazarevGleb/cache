8494b813bc6ca8c0659ac61ffae525ff62055f8f
LazarevGleb <lazarevgb@gmail.com>
2020-10-21 17:29:57 +0300

    Added candidates cache

--- src/test/java/LfuCacheTest.java
+++ src/test/java/LfuCacheTest.java
@@ -24,6 +24,7 @@ class LfuCacheTest extends BaseCacheTest {
         cache.put("4", ob);
         LocalDate now = LocalDate.now();
         cache.put("now", now);
+        cache.put("now", now);
         assertEquals(3, cache.size());
         assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("1", "abc")));
         assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("2", 2L)));
@@ -36,6 +37,31 @@ class LfuCacheTest extends BaseCacheTest {
         cache.put("null", null);
         cache.put("null", null);
         assertEquals(1, cache.size());
+    }
+
+    @Test
+    void testPut_3() {
+        cache.put("1", "abc");
+        cache.put("1", "abc");
+        cache.put("2", 2L);
+        cache.put("2", 2L);
+        cache.put("1", "abc");
+        cache.put("2", 2L);
+        Object ob = new Object();
+        cache.put("4", ob);
+        LocalDate now = LocalDate.now();
+        cache.put("now", now);
+        cache.put("now", now);
+        assertEquals(3, cache.size());
+        assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("1", "abc")));
+        assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("2", 2L)));
+        assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("now", now)));
+        assertFalse(cache.getCacheItemsWithKeys().contains(Map.entry("4", ob)));
+
+        cache.put("4", ob);
+        cache.put("4", ob);
+        assertTrue(cache.getCacheItemsWithKeys().contains(Map.entry("4", ob)));
+        assertFalse(cache.getCacheItemsWithKeys().contains(Map.entry("now", now)));
     }
 
     @Test
